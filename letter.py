8841bd13e497d66f09e157513bfec5a0691be5c5
oumaima <oumaimadjihanebmw@gmail.com>
2026-02-06 20:52:34 +0100

    updates letter.py

--- letter.py
+++ letter.py
@@ -1,1 +1,32 @@
+#les comontaires
+# variables :Think of it like a box: slap on a label, and it can store stuff
 print ('hi girls ')
+# Write code below 💖
+f=31
+c=(f-32)/1.8
+print ('the temperateur in Fahrenheit is :'+str(f)+   '   '   'and in Celsius is : '+str(c))
+#cioncatination tji b +str(var)+ wki tkon bin var ki ykon fih ra9m berkk w jomli tji b had function 
+#bsh f normal f=oumaima wal tji print ('namr is:'+f)3addii
+# Write code below 💖
+mass=91
+height=1.68
+bmi=mass/(height**2)
+print('your bmi is : '+str(bmi))
+#Python uses the input() function to get user input:
+username = input('Enter your name: ')
+print(username)
+#or 
+age = int(input('What is your age? '))
+print(age)
+#Data types: int, float, str, bool.
+#Arithmetic operators: +, -, *, /.
+#The % modulo finds the remainder.
+#The ** exponentiation finds the exponent.
+#The input() function is used to get user input.
+#The int() function converts a value input into an integer number
+# Write code below 💖
+import math
+a=int (input('inter the a which is  the length of a short sid '))
+b=int (input('inter the b which is  the length of another short sid '))
+c=math.sqrt(a**2+b**2)
+print (c)
